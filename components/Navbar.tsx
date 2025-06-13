79d5beb66cbe6fccf4630495ae8c6834ae4e26f1
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-06-13 21:47:49 +0600

    Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+
+const user ={}
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="navbar" >
+      <nav>
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="logo"
+            height={32}
+            width={32}
+          />
+          <h1>SnapCast</h1>
+        </Link>
+        {user && (
+          <figure>
+            <button>
+              <Image src="/assets/images/dummy.jpg" alt="user" width={36} height={36} />
+            </button>
+          </figure>
+        )}
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
