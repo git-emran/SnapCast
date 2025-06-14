f18b25456c564fae222373bff0de406fa1966e78
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-06-14 14:49:35 +0600

    home page works

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,17 @@
 import Header from "@/components/Header";
 import VideoCard from "@/components/VideoCard";
+import { dummyCards } from "@/constants";
 import React from "react";
 
 const Page = () => {
   return (
     <main className="wrapper page">
       <Header title="All videos" subheader="Public Library" />
-      <h1 className="text-2xl font-karla">Welcome to Loom clone</h1>
-      <VideoCard
-        id="1"
-        title="Snapchat"
-        thumbnail="/assets/samples/thumbnail (1).png"
-        createdAt="2025-01-01 06:25:54.435"
-        userImg="/assets/images/jason.png"
-        views={10}
-        visibility="public"
-        duration={156}
-      />
+      <section className="video-grid">
+        {dummyCards.map((card) => (
+          <VideoCard key={card.id} {...card} />
+        ))}
+      </section>
     </main>
   );
 };
--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,11 +1,28 @@
 import Header from "@/components/Header";
+import VideoCard from "@/components/VideoCard";
+import { dummyCards } from "@/constants";
+
+type ParamWithSearch = {
+  params: {
+    id: string;
+    [key: string]: string; // This allows for additional search parameters
+  };
+};
 
 const Page = async ({ params }: ParamWithSearch) => {
-  const { id } = await params;
+  const { id } = params;
   return (
     <div className="wrapper page">
-      <Header subheader="emrn.hossn@gmail.com " title="Emran Web Dev" userImg="/assets/images/dummy.jpg" />
-      USER ID: {id}
+      <Header
+        subheader="emrn.hossn@gmail.com "
+        title="Emran Web Dev"
+        userImg="/assets/images/dummy.jpg"
+      />
+      <section className="video-grid">
+        {dummyCards.map((card) => (
+          <VideoCard key={card.id} {...card} />
+        ))}
+      </section>
     </div>
   );
 };
