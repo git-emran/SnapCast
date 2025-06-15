55ee6e10960c84eeb5341331e8840b6aca9e0781
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-06-15 13:26:14 +0600

    testing

--- components/Header.tsx
+++ components/Header.tsx
@@ -45,7 +45,7 @@ const Header = ({ subheader, title, userImg }: SharedHeaderProps) => {
                 height={16}
                 width={16}
               />
-              <span>Record a video</span>
+              <span>Record a video lol</span>
             </button>
           </div>
         </aside>
