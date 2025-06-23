bf2a81eaa5808090b5edfd7d6dfc20f63d8a5636
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-06-24 00:39:52 +0600

    fixing auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,17 +4,23 @@ import { db } from "@/drizzle/db";
 import { schema } from "@/drizzle/schema";
 import { nextCookies } from "better-auth/next-js";
 
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+
+if (!baseURL) {
+  throw new Error("❌ Missing NEXT_PUBLIC_BASE_URL in environment variables.");
+}
+
 export const auth = betterAuth({
-    database: drizzleAdapter(db, {
-        provider: 'pg',
-        schema,
-    }),
-    socialProviders: {
-        google: {
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
-    plugins: [nextCookies()],
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL!
+  },
+  plugins: [nextCookies()],
+  baseURL,
 });
