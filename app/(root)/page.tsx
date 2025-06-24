fa0a3fcd6276caf5327aa8149fdb568c5d026580
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-06-24 14:07:15 +0600

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,13 +2,6 @@ import Header from "@/components/Header";
 import { getAllVideos } from "@/lib/actions/video";
 import React from "react";
 
-interface SearchParams {
-  searchParams: {
-    query?: string;
-    filter?: string;
-    page?: string | number;
-  };
-}
 
 const Page = async ({ searchParams }: SearchParams) => {
   const { query, filter, page } = await searchParams;
