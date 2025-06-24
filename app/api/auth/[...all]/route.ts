84e82fe5675b6e7c9e18d00d8a5760f947e0faef
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-06-24 15:04:16 +0600

    will this work now

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,19 +1,21 @@
-import { toNextJsHandler } from "better-auth/next-js";
+import aj, {
+  ArcjetDecision,
+  shield,
+  slidingWindow,
+  validateEmail,
+} from "@/lib/arcjet";
+import ip from "@arcjet/ip";
 import { auth } from "@/lib/auth";
-import aj from "@/lib/arcjet";
-import { slidingWindow, validateEmail } from "@arcjet/next";
+import { toNextJsHandler } from "better-auth/next-js";
 import { NextRequest } from "next/server";
-import type { ArcjetDecision } from "@arcjet/next";
-import ip from "@arcjet/next";
 
-//Email Vaildation
 const emailValidation = aj.withRule(
   validateEmail({
     mode: "LIVE",
     block: ["DISPOSABLE", "INVALID", "NO_MX_RECORDS"],
   })
 );
-//Rate Limit
+
 const rateLimit = aj.withRule(
   slidingWindow({
     mode: "LIVE",
@@ -23,10 +25,18 @@ const rateLimit = aj.withRule(
   })
 );
 
+const shieldValidation = aj.withRule(
+  shield({
+    mode: "LIVE",
+  })
+);
+
 const protectedAuth = async (req: NextRequest): Promise<ArcjetDecision> => {
-  const session = await auth.api.getSession({ headers: req.headers });
+  const session = await auth.api.getSession({
+    headers: req.headers,
+  });
   let userId: string;
-  if (session?.user?.id) {
+  if (session?.user.id) {
     userId = session.user.id;
   } else {
     userId = ip(req) || "127.0.0.1";
@@ -34,10 +44,17 @@ const protectedAuth = async (req: NextRequest): Promise<ArcjetDecision> => {
   if (req.nextUrl.pathname.startsWith("/api/auth/sign-in")) {
     const body = await req.clone().json();
     if (typeof body.email === "string") {
-      return emailValidation.protect(req, { email: body.email });
+      return emailValidation.protect(req, {
+        email: body.email,
+      });
     }
   }
-  return rateLimit.protect(req, { fingerprint: userId });
+  if (!req.nextUrl.pathname.startsWith("/api/auth/sign-out")) {
+    return rateLimit.protect(req, {
+      fingerprint: userId,
+    });
+  }
+  return shieldValidation.protect(req);
 };
 
 const authHandlers = toNextJsHandler(auth.handler);
@@ -48,16 +65,15 @@ export const POST = async (req: NextRequest) => {
   const decision = await protectedAuth(req);
   if (decision.isDenied()) {
     if (decision.reason.isEmail()) {
-      throw new Error("Email validation Failed");
+      throw new Error("Email validation failed");
     }
-
     if (decision.reason.isRateLimit()) {
-      throw new Error("Rate Limit Exceeded");
+      throw new Error("Rate limit exceeded");
     }
-
     if (decision.reason.isShield()) {
-      throw new Error("Sheild turned on, Better Luck next time");
+      throw new Error("Shield validation failed");
     }
   }
+
   return authHandlers.POST(req);
 };
